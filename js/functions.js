50bb3a8cc59708d985ad7f8039ba5db0f0ece139
Varun Chakka <Varun.Chakka@outlook.com>
2020-08-02 18:17:02 -0700

    IMAGES YAYAY!

--- js/functions.js
+++ js/functions.js
@@ -18,3 +18,11 @@ function addItem() {
     list.children[i].onclick = removeItem;
    }
   }
+  function addImage() {
+      var img = new Image();
+      img.src = document.getElementById("imageURL").value;
+      img.height = 400
+      img.style.cssFloat="right"
+      img.style.cssFloat="left"
+      document.getElementById('body').appendChild(img);
+    }
